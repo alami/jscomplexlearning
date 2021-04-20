82a186de72a7a469a88d6e8a0e8630a6883bbfc5
alami <alami@km.ru>
2021-04-20 18:31:38 +0500

    ch4: Array.from(colors).forEach(color => color.addEventListener('click',handleColorClick))

--- vanilla/app.js
+++ vanilla/app.js
@@ -2,26 +2,47 @@
 // https://github.com/agolosnichenko
 // https://github.com/EasyCoders-ru/paintjs
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d')
+const colors = document.getElementsByClassName('jsColor');
+
+canvas.width = 700
+canvas.height = 700
+
+ctx.lineWidth = 2.5
+ctx.strokeStyle  = '#2c2c2c'
+
 let painting = false
 
 function StopPainting(event){
     painting = false
 }
+function StartPainting(event){
+    painting = true
+}
 function onMouseMove(event){
     x = event.offsetX
     y = event.offsetY
-    console.log(x,y)
+    if (!painting) {
+        ctx.beginPath()
+        ctx.moveTo(x,y)
+    } else {
+        ctx.lineTo(x,y)
+        ctx.stroke()
+    }
 }
 function onMouseDown(event){
     painting = true
 }
-function onMouseUp(event){
-    StopPainting(event)
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor
+    ctx.strokeStyle = color
 }
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mouseup', StopPainting);
     canvas.addEventListener('mouseleave', StopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click',handleColorClick))
