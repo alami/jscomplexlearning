199217a5b9c06193bdcc0c78424e7e236017a27c
alami <alami@km.ru>
2021-04-20 16:36:53 +0500

    vanilla bonus : paint js

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,3 +1,12 @@
 //  https://meyerweb.com/eric/tools/css/reset/
 // https://github.com/agolosnichenko
 // https://github.com/EasyCoders-ru/paintjs
+const canvas = document.getElementById('jsCanvas');
+
+function onMouseMove(event){
+    console.log(canvas)
+}
+
+if (canvas) {
+    canvas.addEventListener('mousemove', onMouseMove);
+}
