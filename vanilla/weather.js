06b4ed826c718fc0c65299875e901a249ccc8fba
Aminov <alami@tshtt.uz>
2021-04-20 09:28:46 +0500

    vanilla js : weather : geolocation v2

--- vanilla/weather.js
+++ vanilla/weather.js
@@ -4,11 +4,11 @@ const COORDS_LS = 'coords'
 function geoSuccessHandler(position) {
     console.log(position)
 }
-function geoSuccessHandler() {
+function geoErrorHandler() {
     console.log('Error in finding of position !')
 }
 function askForCoords() {
-    navigator.geolocation.getCurrentPosition(geoSuccessHandler, geoSuccessHandler)
+    navigator.geolocation.getCurrentPosition(geoSuccessHandler, geoErrorHandler)
 }
 function getCoords() {
     const coords = localStorage.getItem(COORDS_LS)
