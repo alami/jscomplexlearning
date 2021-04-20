31e8c22a2e275fb10f0f6975c4017bc771b78d7d
alami <alami@km.ru>
2021-04-20 14:10:35 +0500

    vanilla js : weather fetch-json

--- vanilla/weather.js
+++ vanilla/weather.js
@@ -1,5 +1,15 @@
-const COORDS_LS = 'coords'   //https://openweathermap.org
+const COORDS_LS = 'coords'
 
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric `)
+        .then(function (response) {
+            return (response.json())
+        }).then(function (json) {
+        console.log(json)
+    })
+}
+
+const API_KEY = 'bdf3d54816461204b5749e67607bcc56' //https://openweathermap.org
 function saveCoords(positionObj) {
     localStorage.setItem(COORDS_LS, JSON.stringify(positionObj))
 }
@@ -11,6 +21,7 @@ function geoSuccessHandler(position) {
         longitude
     }
     saveCoords(positionObj)
+    getWeather(latitude, longitude)
 }
 function geoErrorHandler() {
     console.log('Error in finding of position !')
@@ -23,7 +34,9 @@ function getCoords() {
     if (coords === null) {
         askForCoords ()
     } else {
-
+        const locadedCoords = JSON.parse(coords)
+        console.log(locadedCoords)
+        getWeather(locadedCoords.latitude, locadedCoords.longitude)
     }
 }
 
