396387d5272375ac36df818ca38db46d842e9950
alami <alami@km.ru>
2021-04-20 17:01:00 +0500

    vanilla bonus : paint mouse events

--- vanilla/app.js
+++ vanilla/app.js
@@ -2,11 +2,26 @@
 // https://github.com/agolosnichenko
 // https://github.com/EasyCoders-ru/paintjs
 const canvas = document.getElementById('jsCanvas');
+let painting = false
 
+function StopPainting(event){
+    painting = false
+}
 function onMouseMove(event){
-    console.log(canvas)
+    x = event.offsetX
+    y = event.offsetY
+    console.log(x,y)
+}
+function onMouseDown(event){
+    painting = true
+}
+function onMouseUp(event){
+    StopPainting(event)
 }
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', onMouseDown);
+    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mouseleave', StopPainting);
 }
